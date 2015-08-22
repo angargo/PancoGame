3c6a7fb6a4c9cfd381b476be5eb5f546c8f90eeb
angargo <angelgarciagmz@gmail.com>
2015-08-22 04:48:03 +0200

    Minor fixes in world.

--- Serda/world.cpp
+++ Serda/world.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 #include "world.h"
 
@@ -13,6 +14,7 @@ World::~World() {}
 
 const Component& World::getComponent(const Entity& entity,
                                      Component::Id component_id) const {
+  assert(entity.components[component_id]);
   const int index = entity.component_indices[component_id];
   switch (component_id) {
     case Component::POSITION: return position_components[index]; break;
@@ -22,17 +24,17 @@ const Component& World::getComponent(const Entity& entity,
   }
 }
 
-const Component &World::getComponent(id_type entity_id,
+const Component& World::getComponent(id_type entity_id,
                                      Component::Id component_id) const {
   return getComponent(entities.at(entity_id), component_id);
 }
 
-Component &World::mutableComponent(const Entity &entity,
+Component& World::mutableComponent(const Entity &entity,
                                    Component::Id component_id) {
   return const_cast<Component&>(getComponent(entity, component_id));
 }
 
-Component &World::mutableComponent(id_type entity_id,
+Component& World::mutableComponent(id_type entity_id,
                                    Component::Id component_id) {
   return mutableComponent(entities.at(entity_id), component_id);
 }
