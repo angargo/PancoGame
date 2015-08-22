bdccbd7a8c5f3e1ffa3ae200e88f4e406ebcbfe2
Albert Martinez <albertmg7@gmail.com>
2015-08-22 04:19:38 +0200

    Put enum inside Component class

--- Serda/entity.cpp
+++ Serda/entity.cpp
@@ -1,9 +1,17 @@
 #include "entity.h"
 
-Entity::Entity(const id_type id) : id(id), components(), component_indices() {}
-
+// Base component.
 Component::Component(const id_type entity_id) : entity_id(entity_id) {}
 Component::~Component() {}
 
+// Entity.
+Entity::Entity(const id_type id) : id(id), components(), component_indices() {}
+
+bool Entity::hasComponent(Component::ComponentId id) {
+  return components[id];
+}
+
+// Components.
 PositionComponent::PositionComponent(const id_type entity_id, float x, float y)
-    : Component(entity_id), x(x), y(y) {}
+    : Component(entity_id), x(x), y(y) {
+}
--- Serda/entity.h
+++ Serda/entity.h
@@ -6,29 +6,29 @@
 
 typedef int id_type;
 
-// Ids for all components.
-enum ComponentId {
-  POSITION = 0,
-  NUM_IDS = 1,
+// Component base class.
+class Component {
+public:
+  enum ComponentId {
+    POSITION = 0,
+    NUM_IDS = 1,
+  };
+
+  explicit Component(const id_type entity_id);
+  virtual ~Component() = 0;
+
+  const id_type entity_id;
 };
 
 // Entity class.
 class Entity {
 public:
   explicit Entity(const id_type id);
+  bool hasComponent(Component::ComponentId id);
 
   const id_type id;
-  std::bitset<ComponentId::NUM_IDS> components;
-  std::array<int, ComponentId::NUM_IDS> component_indices;
-};
-
-// Component base class.
-class Component {
-public:
-  explicit Component(const id_type entity_id);
-  virtual ~Component() = 0;
-
-  const id_type entity_id;
+  std::bitset<Component::NUM_IDS> components;
+  std::array<int, Component::NUM_IDS> component_indices;
 };
 
 // Sample PositionComponent.
