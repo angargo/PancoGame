4fb2bf23ccf72eaa4512f9e600f4eafdc4b73c4b
Albert Martinez <albertmg7@gmail.com>
2015-08-22 05:51:46 +0200

    Added components and entities to world

---
+++ Serda/world.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include "world.h"
+
+World::World()
+    : entities(),
+      position_components(),
+      speed_components(),
+      render_components() {
+};
+
+World::~World() {}
+
+const Component& World::getComponent(const Entity& entity,
+                                     Component::Id component_id) const {
+  const int index = entity.component_indices[component_id];
+  switch (component_id) {
+    case Component::POSITION: return position_components[index]; break;
+    case Component::SPEED: return speed_components[index]; break;
+    case Component::RENDER: return render_components[index]; break;
+    default: std::cerr << "Error: invalid component id\n"; exit(0); break;
+  }
+}
+
+const Component &World::getComponent(id_type entity_id,
+                                     Component::Id component_id) const {
+  return getComponent(entities.at(entity_id), component_id);
+}
+
+Component &World::mutableComponent(const Entity &entity,
+                                   Component::Id component_id) {
+  return const_cast<Component&>(getComponent(entity, component_id));
+}
+
+Component &World::mutableComponent(id_type entity_id,
+                                   Component::Id component_id) {
+  return mutableComponent(entities.at(entity_id), component_id);
+}
--- Serda/world.h
+++ Serda/world.h
@@ -1,15 +1,31 @@
 #ifndef SERDA_WORLD_H
 #define SERDA_WORLD_H
 
+#include <vector>
 #include <unordered_map>
 
 #include "entity.h"
 
 class World {
+public:
+  World();
+  ~World();
+
+  const Component& getComponent(const Entity& entity,
+                                Component::Id component_id) const;
+  const Component& getComponent(id_type entity_id,
+                                Component::Id component_id) const;
+  Component& mutableComponent(const Entity& entity, Component::Id component_id);
+  Component& mutableComponent(id_type entity_id, Component::Id component_id);
+
+private:
   // TODO: cache problems, maybe.
   std::unordered_map<id_type, Entity> entities;
 
   // Components go here.
+  std::vector<PositionComponent> position_components;
+  std::vector<SpeedComponent> speed_components;
+  std::vector<RenderComponent> render_components;
 };
 
-#endif // SERDA_WORLD_H
+#endif  // SERDA_WORLD_H
