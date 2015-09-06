0274062a2351d8be297c30a10d5bfb6e1b42e6da
angargo <angelgarciagmz@gmail.com>
2015-09-06 21:49:27 +0200

    Improve link input.

--- Serda/media/lua/link_input.lua
+++ Serda/media/lua/link_input.lua
@@ -4,31 +4,19 @@ local s = vars[id]; -- Script specific variables in this table.
 
 local mxSpeed = 100;
 
-local up
-function up(entity, pressed)
-  addSpeed(entity, 0, mxSpeed * (pressed and -1 or 1));
-end
-
-local down
-function down(entity, pressed)
-  addSpeed(entity, 0, mxSpeed * (pressed and 1 or -1));
-end
-
-local right
-function right(entity, pressed)
-  addSpeed(entity, mxSpeed * (pressed and 1 or -1), 0);
-end
-
-local left
-function left(entity, pressed)
-  addSpeed(entity, mxSpeed * (pressed and -1 or 1), 0);
-end
-
 local input
 function input(entity, key, pressed)
-  if key == "Up" then up(entity, pressed) end
-  if key == "Down" then down(entity, pressed) end
-  if key == "Right" then right(entity, pressed) end
-  if key == "Left" then left(entity, pressed) end
+  if s[entity] == nil then
+    s[entity] = {}
+    local v = s[entity];
+    v.up, v.down, v.right, v.left = 0, 0, 0, 0
+  end
+
+  local v = s[entity];
+  if key == "Up" then v.up = pressed and 1 or 0 end
+  if key == "Down" then v.down = pressed and 1 or 0 end
+  if key == "Right" then v.right = pressed and 1 or 0 end
+  if key == "Left" then v.left = pressed and 1 or 0 end
+  setSpeed(entity, (v.right - v.left) * mxSpeed, (v.down - v.up) * mxSpeed);
 end
 game[id] = input
